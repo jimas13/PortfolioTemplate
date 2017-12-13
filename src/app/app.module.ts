39b38c21ff4b4d7cfbbcb0bccfa2e3844a67db64
Dimitris Pivovarov <d.pivovarov@sieben.gr>
2017-12-13 18:01:27 +0200

    restyle css-->bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { NavbarModule } from './navbar/navbar.module';
-import { FooterModule } from './footer/footer.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     AboutComponent,
     EducationComponent,
     SkillsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NavbarModule,
-    FooterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { NavbarModule } from './app/navbar/navbar.module';
-import { FooterModule } from './app/footer/footer.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(NavbarModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
